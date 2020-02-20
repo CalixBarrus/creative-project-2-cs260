7c86a87f8aad92b015709a1d621edc7cb250cdb4
Calix Barrus <calix.barrus@gmail.com>
2020-02-19 17:05:04 -0700

    Added random day selection using moment, parses more of the returned JSON.

--- scripts.js
+++ scripts.js
@@ -1,11 +1,18 @@
 
 document.getElementById("button").addEventListener("click", (event) => {
-    const url = "http://apodapi.herokuapp.com/api/";
+    let url = "http://apodapi.herokuapp.com/api/?date=";
+    let randomInt = Math.floor(Math.random() * 9000);
+    url += moment().add(-randomInt, 'days').format("YYYY-MM-DD")
     fetch(url)
     .then(function(response) {
         return response.json();
     }).then(function(json) {
-        let results = "";
+        document.getElementById("title").innerHTML = json.title;
+
+        document.getElementById("date").innerHTML = json.date;
+
         document.getElementById("photo").src = json.hdurl;
+
+        document.getElementById("description").innerHTML = json.description;
     });
 });
